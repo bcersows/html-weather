af7dd0f1b9ced17173fc7d5f0d0bac1ba048af70
bcersows <bcersows@xlab.t-iot.de>
2017-03-04 18:19:09 +0100

    Removed the dependency on JQuery. (Even though the code is now significantly longer and worse to read. Worth it!) Also enhanced the README.md a bit. Increasing release to v1.2.0.

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # html-weather
+[![GitHub version](https://badge.fury.io/gh/bcersows%2Fhtml-weather.svg)](https://badge.fury.io/gh/bcersows%2Fhtml-weather)
+
 Small and simple weather overlay using (mostly) CSS.
 
 ## Demo
 Check it out [here](http://codepen.io/bcersows/full/vgLEEg).
+
+## Installation
+Available via bower: `bower install --save` or by download the ZIP
+[directly from GitHub](https://github.com/bcersows/html-weather/archive/master.zip).
 
 ## Usage
 To use it, simply import the JS and CSS files from the dist folder.
@@ -19,6 +25,7 @@ controlling the weather. For that use one of the following functions:
 * **setSun**: Some sunbeams
 * **setSnow**: Snowflakes for the winter feeling
 * **setRain**: Darn autumn
+* **setWind**: For all the wind surfers and kiters out there!
 
 e.g.: `weather.setSnow();`
 
--- js/html-weather.js
+++ js/html-weather.js
@@ -8,6 +8,7 @@
 "use strict";
 
 /**
+ * The control object for setting the weather.
  * Functions:
  *   setSun
  *   setSnow
@@ -16,11 +17,28 @@
  *   setClear
  */
 const weather = new function() {
+  /**
+   * Set a sunny weather.
+   */
   this.setSun = showSun;
+  /**
+   * Let snowflakes float down.
+   */
   this.setSnow = showSnow;
+  /**
+   * A cuddly rainy weather.
+   */
   this.setRain = showRain;
+  /**
+   * Wind's howling outside.
+   */
   this.setWind = showWind;
+  /**
+   * Clear all weather effects.
+   */
   this.setClear = showClear;
+  
+  var initializedWeatherAreas;
   
   var interval;
   var intervalCounter;
@@ -34,9 +52,21 @@ const weather = new function() {
     "wind"
   ];
   
+  window.onbeforeunload = closingCode;
+  function closingCode(){
+    // do something...
+    console.log("lala");
+    return null;
+  }
+  
   function addElements(amount, duration) {
     for ( var i = 0;i<amount;i++ ) {
-      $('.weather>.weather-wrapper').append('<div class="weather-element"></div>');
+      var div = document.createElement("div");
+      div.innerHTML = '<div class="weather-element"></div>';
+      Array.prototype.forEach.call(initializedWeatherAreas, function(weatherWrapper, i){
+        weatherWrapper.appendChild( div.firstChild );
+      });
+      div = null;
     }
     if ( intervalCounter >= intervalMax ) {
       clearInterval(interval);
@@ -69,11 +99,20 @@ const weather = new function() {
     setWeatherInterval(10, 1, 1);
   }
   
-  function setWeather(typeId) {
-    var $weather = $('.weather>.weather-wrapper');
-    $weather.removeClass("snow sun rain wind")
-      .addClass(weathers[typeId]);
-    $weather.children('.weather-element').remove();
+  function setWeather(typeId) {    
+    // Reset the current weather.
+    Array.prototype.forEach.call(initializedWeatherAreas, function(weatherWrapper, i){
+      weatherWrapper.classList.remove("rain", "snow", "sun", "wind");
+      weatherWrapper.classList.add(weathers[typeId]);
+      
+      // Remove existing weather elements.
+      var children = weatherWrapper.children;
+      Array.prototype.forEach.call(children, function(child, i){
+        if ( child.classList.contains("weather-element") ) {
+          child.parentNode.removeChild(child);
+        }
+      });
+    });
   }
   
   function setWeatherInterval(elementCount, delay, maxDuration) {
@@ -88,10 +127,30 @@ const weather = new function() {
     }, delay*1000);
   }
   
-  var $weather = $('.weather');
-  if ( $weather.children('.weather-wrapper').length<=0 ) {
-    $weather.append('<div class="weather-wrapper"></div>');
-  }
-  $weather = null;
+  /**
+   * Initialize the existing weather containers.
+   */
+  function initialize() {
+    initializedWeatherAreas = [];
+    var weathers = document.querySelectorAll('.weather');
+    
+    // Iterate through all weather containers and add the .weather-wrapper,
+    //  if necessary.
+    Array.prototype.forEach.call(weathers, function(weather, i){
+        var wrapper = weather.querySelector('.weather-wrapper');
+        if ( wrapper === null ) {
+          var wrapper = document.createElement("div");
+          wrapper.className = "weather-wrapper";
+          weather.appendChild( wrapper );
+        }
+        initializedWeatherAreas.push(wrapper);
+        wrapper = null;
+    });
+    
+    weathers = null;
+  };
+  
+  initialize();
+  
   return this;
 };
